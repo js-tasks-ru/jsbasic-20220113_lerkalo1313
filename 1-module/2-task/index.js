266cd3dccbc9856260fdcd30ac08c6a1ba673392
Valeriya Loskutova <lerkalo1313@gmail.com>
2022-01-20 22:28:27 +0300

    1-5 tasks 1-module

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -2,7 +2,7 @@
  * Эту функцию трогать не нужно
  */
 function print(text) {
-  console.log(text);
+  console.log(text)
 }
 
 /**
@@ -10,15 +10,27 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (!name) {
+    return false
+  }
+
+  if (name.includes(' ')) {
+    return false
+  }
+
+  if (name.length < 4) {
+    return false
+  }
+
+  return true
 }
 
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+  let userName = prompt('Введите ваше имя')
 
   if (isValid(userName)) {
-    print(`Welcome back, ${userName}!`);
+    print(`Welcome back, ${userName}!`)
   } else {
-    print('Некорректное имя');
+    print('Некорректное имя')
   }
 }
