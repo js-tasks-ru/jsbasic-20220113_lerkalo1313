182a440f211934e7f000a86cb527d7a64623d0b7
Valeriya Loskutova <lerkalo1313@gmail.com>
2022-02-02 21:04:04 +0300

    3-module

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
-  // ваш код...
+  const result = []
+
+  users.forEach((user) => {
+    if (user.age <= age) {
+     result.push(`${user.name}, ${user.balance}`)
+    }
+  })
+  return result.join('\n')
 }
